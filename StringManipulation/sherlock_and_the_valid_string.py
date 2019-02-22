4d07aadea0f0f3c4fc5c154ba87f78eb87d3bcb8
Pier <madstrum@gmail.com>
2019-02-22 23:21:23 +0800

    Commented

--- StringManipulation/sherlock_and_the_valid_string.py
+++ StringManipulation/sherlock_and_the_valid_string.py
@@ -6,6 +6,7 @@ import random
 import re
 import sys
 
+
 def getFreqCountDict(s):
     adict = {}
     for char in s:
@@ -16,32 +17,37 @@ def getFreqCountDict(s):
     print(adict)
     return adict
 
+
 # Complete the isValid function below.
 def isValid(s):
     sdict = getFreqCountDict(s)
     sdict_list = list(sdict.values())
     freq_cnt_dict = getFreqCountDict(sdict_list)
-    if len(freq_cnt_dict) > 2:
+    if len(freq_cnt_dict) > 2:  # more than 2 frequencies of char, no way ie. aabbbcccc
         return "NO"
-    elif len(freq_cnt_dict) == 1:
+    elif len(freq_cnt_dict) == 1:  # ie. a
         return "YES"
-    else:
+    else:  # there's just 2 entries in this dict, get them
         akey = list(freq_cnt_dict)[0]
         bkey = list(freq_cnt_dict)[1]
         aval = list(freq_cnt_dict.values())[0]
         bval = list(freq_cnt_dict.values())[1]
 
-        if aval > bval: # aabbccc | a:2 b:2 c:3 | 2:2 3:1
+        # Eg. aabbccc | sdict a:2 b:2 c:3 | freq_cnt_dict akey 2:aval 2   bkey 3:bval 1
+
+        if aval > bval:  # then b is the one from which to remove a char
+            # 2 cases, (1) remove 1 char from b such that b is on par with a
+            #          (2) remove 1 char (that has 1 occurance)
             if bkey - akey == 1 or (bkey == 1 and bval == 1):
                 return "YES"
             else:
                 return "NO"
-        elif bval > aval:
+        elif bval > aval:  # this is just exact mirror
             if akey - bkey == 1 or (akey == 1 and aval == 1):
                 return "YES"
             else:
                 return "NO"
-        else: # aval == bval
+        else:  # aval == bval   ie. aabbcccddd a:2 b:2 c:3 d:3
             return "NO"
 
 
